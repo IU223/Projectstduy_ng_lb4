3a3dbfef568a2ddf47e34b97656ebf093e73b6e8
Yongfeng YF Xu <Yongfeng_YF_Xu@wistron.com>
2026-05-28 15:00:00 +0800

    清空不必要的项目文件

---
+++ test/test.js
@@ -0,0 +1,6 @@
+"use strict";
+let students = [
+    { name: "Alice", age: 20, grade: 85 },
+    { name: "Bob", age: 22, grade: 90 },
+];
+console.log(students);
---
+++ test/test.ts
@@ -0,0 +1,6 @@
+interface student { name: string, age: number, grade: number }
+
+let students: student[] = [
+    { name: "Alice", age: 20, grade: 85 },
+    { name: "Bob", age: 22, grade: 90 },]
+console.log(students)
